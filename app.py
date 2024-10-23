95943541ea77f95adcb9dac7b68a80b1c8460bd8
Jason Spitkoski <jason@digitalwaters.org>
2024-10-23 13:29:57 -0400

    updating delete call
    remove all test records from new device

--- app.py
+++ app.py
@@ -228,8 +228,8 @@ async def get_data(
 # GET method to retrieve data
 @app.get('/deleteData/')
 async def deleteData(IDtoDelete: int = 0):
-    #return JSONResponse(content={"message": "Delete Disabled"}, status_code=200)
-    return deleteRowsAndS3Data(IDtoDelete)
+    return JSONResponse(content={"message": "Delete Disabled"}, status_code=200)
+    #return deleteRowsAndS3Data(IDtoDelete)
 
 def deleteS3Object(s3_url):
     """Delete the corresponding object from S3 based on its URL."""
@@ -259,7 +259,7 @@ def deleteRowsAndS3Data(IDtoDelete):
         # Build filters
         filters = []
         #filters.append(ImageRecord.id == IDtoDelete)
-        filters.append(ImageRecord.latitude == 999.0)
+        filters.append(ImageRecord.deviceID == "000000002133dded")
         query = query.filter(and_(*filters))
         results = query.all()
 
