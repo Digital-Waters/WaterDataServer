0450c41055dc4a86bb7338e14b254cf37c52fcca
Jason Spitkoski <jason@digitalwaters.org>
2024-11-07 18:27:13 -0500

    fix type when geting env vars

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import os
 
 
 app = FastAPI()
-bucketName = create_engine(os.getenv("S3_BUCKETNAME"))
+bucketName = os.getenv("S3_BUCKETNAME")
 engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
